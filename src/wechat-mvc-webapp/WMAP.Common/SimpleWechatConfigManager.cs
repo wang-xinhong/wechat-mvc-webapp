e57d1e83952274c9cf7c9797286993abf5b3303e
gnaws <gnaws@hotmail.com>
2015-04-19 06:40:51 +0800

    keep file move

---
+++ src/wechat-mvc-webapp/WMAP.Common/SimpleWechatConfigManager.cs
@@ -0,0 +1,48 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMAP.Interfaces;
+
+namespace WMAP.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SimpleWechatConfigManager
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(SimpleWechatConfigManager));
+
+
+        private static SimpleWechatConfigManager instance;
+
+        public SimpleWechatConfigManager()
+        {
+            if (instance == null)
+                instance = new SimpleWechatConfigManager();
+        }
+
+        public static SimpleWechatConfigManager GetInstance()
+        {
+            return instance;
+        }
+
+
+        /// <summary>
+        /// 获取指定的AppID对应的相关配置
+        /// </summary>
+        /// <param name="AppID"></param>
+        /// <returns></returns>
+        public IWechatConfig FetchSelectedConfig(String AppID)
+        {
+            if (String.IsNullOrWhiteSpace(AppID))
+            {
+                throw new ArgumentException(@"AppID");
+            }
+
+            return new SimpleWechatConfig();
+        }
+    }
+}
